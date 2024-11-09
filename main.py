08d3da8219625cc068bbb9d5735eb81719e39564
tero51 <akmalmagistr@gmail.com>
2024-11-09 04:14:18 +0300

    add some stuff

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ import random
 
 n = random.randint(3, 20)
 
+# простенький вариант
+# def find_code():
+#     for i in range(1, int(n/2) + 1):
+#         for j in range(1, n):
+#             if i == j:
+#                 continue
+#             if i + j == n:
+#                 print(i, "+",j)
+# print(n)
+# find_code()
+
+result = []
+
 def find_code():
     for i in range(1, int(n/2) + 1):
         for j in range(1, n):
             if i == j:
                 continue
             if i + j == n:
-                result = print(i,j)
-    return result
+                result.append(i)
+                result.append("+")
+                result.append(j)
 print(n)
 find_code()
+print(*result)
